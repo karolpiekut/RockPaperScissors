425bc923847bf4f02bda7478329020b6c12a35e1
kedemah <karol.piekut@pm.me>
2022-09-07 07:15:00 +0100

    Add HTML game structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,69 @@
+//get random computer choice
 function getComputerChoice() {
-	const availableChoices = ["Rock", "Paper", "Scissors"];
-	return availableChoices[Math.floor(Math.random() * 3)];
+    const availableChoices = ["Rock", "Paper", "Scissors"];
+    return availableChoices[Math.floor(Math.random() * 3)];
 }
 
+//logic draft
 function compareVsUser(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Rock") {
         console.log("It's a draw!");
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
         console.log("You have lost this round!");
-        computerScore ++;
+        computerScore++;
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         console.log("You have won this round!");
-        userScore ++;
+        userScore++;
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
         console.log("it's a draw!");
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         console.log("You have won this round!");
-        userScore ++;
+        userScore++;
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         console.log("You have lost this round!");
-        computerScore ++;
+        computerScore++;
     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
         console.log("it's a draw!");
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         console.log("You have won this round!");
-        userScore ++;
+        userScore++;
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         console.log("You have lost this round!");
-        computerScore ++;
+        computerScore++;
     }
 
 }
 
-let userScore = 0;
-let computerScore = 0;
-let gameCount = 0;
-
-computerChoice = getComputerChoice();
-userChoice = "Rock";
+let userChoice = document.getElementById("Rock").value;
 
 
+console.log(`User choice: ${userChoice}`);
 
-for (let i = 0; i < 5; i++){
+const messageBanner = document.getElementById("messageBanner");
+const gameCount = document.getElementById("gameCount");
+const userScore = document.getElementById("userScore");
+const computerScore = document.getElementById("computerScore");
+
+
+
+
+
+
+//working progress
+//let userScore = 0;
+//let computerScore = 0;
+//let gameCount = 0;
+
+let computerChoice = getComputerChoice();
+
+
+/*
+
+for (let i = 0; i < 5; i++) {
     console.log(compareVsUser(userChoice, getComputerChoice()));
 }
 
-
+*/
 
 
 
