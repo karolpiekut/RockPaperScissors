88cb1b92638ad20280feb4d627a9f282229b4145
kedemah <karol.piekut@pm.me>
2022-09-09 18:51:24 +0100

    Update JS functions to allow a single game

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,54 @@
-//get random computer choice
-function getComputerChoice() {
-    const availableChoices = ["Rock", "Paper", "Scissors"];
-    return availableChoices[Math.floor(Math.random() * 3)];
-}
+const messageBanner = document.querySelector("#messageBanner");
+const gameCountLabel = document.querySelector("#gameCountLabel");
+const userScoreLabel = document.querySelector("#userScoreLabel");
+const computerScoreLabel = document.querySelector("#computerScoreLabel");
+const compChoice = document.querySelector("#whatComputerPicked");
 
-//logic draft
-function compareVsUser(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Rock") {
-        console.log("It's a draw!");
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        console.log("You have lost this round!");
-        computerScore++;
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log("You have won this round!");
-        userScore++;
-    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        console.log("it's a draw!");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        console.log("You have won this round!");
-        userScore++;
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        console.log("You have lost this round!");
-        computerScore++;
-    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        console.log("it's a draw!");
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        console.log("You have won this round!");
-        userScore++;
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        console.log("You have lost this round!");
-        computerScore++;
+function userSelect(par) {
+
+    let userScore = 0;
+    let computerScore = 0;
+    let gameCount = 0;
+    let computerChoice = getComputerChoice();
+
+    function getComputerChoice() {
+        const availableChoices = ["Rock", "Paper", "Scissors"];
+        return availableChoices[Math.floor(Math.random() * 3)];
     }
 
+    function compareVsUser(playerSelection, computerSelection) {
+        if (playerSelection === "Rock" && computerSelection === "Rock") {
+            messageBanner.innerHTML = "It's a draw!";
+        } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+            messageBanner.innerHTML = "You have lost this round!";
+            computerScore++;
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            messageBanner.innerHTML = "You have won this round!";
+            userScore++;
+        } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+            messageBanner.innerHTML = "It's a draw!";
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            messageBanner.innerHTML = "You have won this round!";
+            userScore++;
+        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            messageBanner.innerHTML = "You have lost this round!";
+            computerScore++;
+        } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+            messageBanner.innerHTML = "It's a draw!";
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            messageBanner.innerHTML = "You have won this round!";
+            userScore++;
+        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            messageBanner.innerHTML = "You have lost this round!";
+            computerScore++;
+        }
+    }
+
+    gameCount ++;
+    compareVsUser(par, computerChoice);
+    compChoice.innerHTML = computerChoice;
+    gameCountLabel.innerHTML = gameCount;
+    userScoreLabel.innerHTML = userScore;
+    computerScoreLabel.innerHTML = computerScore;
+
 }
-
-let rock = document.querySelector("#rock");
-let paper = document.querySelector("#paper");
-let scissors = document.querySelector("#scissors");
-
-console.log(rock);
-console.log(paper);
-console.log(scissors);
-
-
-
-//console.log(`User choice: ${userChoice}`);
-
-//const messageBanner = document.querySelector("#messageBanner");
-//const gameCount = document.querySelector("#gameCount");
-//const userScore = document.querySelector("#userScore");
-//const computerScore = document.querySelector("#computerScore");
-
-
-
-
-
-
-//work in progress
-let userScore = 0;
-let computerScore = 0;
-let gameCount = 0;
-
-let computerChoice = getComputerChoice();
-
-
-/*
-
-for (let i = 0; i < 5; i++) {
-    console.log(compareVsUser(userChoice, getComputerChoice()));
-}
-
-*/
-
-
-
-//console.log(`User score: ${userScore}`);
-//console.log(`Computer score: ${computerScore}`);
-//console.log(`Game count: ${gameCount}`);
