1f67ce93b7ce92750eedea7fc06d83dc526a7aec
kedemah <karol.piekut@pm.me>
2022-09-13 07:13:05 +0100

    Update JS for the game, making it playable

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 The Odin Project Rock Paper Scissors Assignment 
 
 The goal is to create a fully functional Rock Paper Scissors game. The project suggests that the user propmpt should be used, but I rather have user input through a press of a button.
+
+Outstanding items:
+- create a fix where the game ends after the game count reaches 5 and not when a user selection button is pressed for the 6th time
+- replace buttons with images
+- make css pretty
--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,79 @@
+//store labels in the constants
 const messageBanner = document.querySelector("#messageBanner");
 const gameCountLabel = document.querySelector("#gameCountLabel");
 const userScoreLabel = document.querySelector("#userScoreLabel");
 const computerScoreLabel = document.querySelector("#computerScoreLabel");
 const compChoice = document.querySelector("#whatComputerPicked");
 
+//game variables
+let userScore = 0;
+let computerScore = 0;
+let gameCount = 0;
+
+//make a random choice for the computer
+function getComputerChoice() {
+    const availableChoices = ["Rock", "Paper", "Scissors"];
+    return availableChoices[Math.floor(Math.random() * 3)];
+}
+
+//game logic
+function compareVsUser(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        messageBanner.innerHTML = "It's a draw!";
+        gameCount++;
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        messageBanner.innerHTML = "You Lose! Paper beats Rock";
+        computerScore++;
+        gameCount++;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        messageBanner.innerHTML = "You Win! Rock beats Scissors";
+        userScore++;
+        gameCount++;
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        messageBanner.innerHTML = "It's a draw!";
+        gameCount++;
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        messageBanner.innerHTML = "You Win! Paper beats Rock";
+        userScore++;
+        gameCount++;
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        messageBanner.innerHTML = "You Lose! Scissors beat Paper";
+        computerScore++;
+        gameCount++;
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        messageBanner.innerHTML = "It's a draw!";
+        gameCount++;
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        messageBanner.innerHTML = "You Win! Scissors beat Paper";
+        userScore++;
+        gameCount++;
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        messageBanner.innerHTML = "You Lose! Rock beats Scissors";
+        computerScore++;
+        gameCount++;
+    }
+}
+
+
+//execution of the game after a button is pressed
 function userSelect(par) {
+    if (gameCount < 5) {
+        let computerChoice = getComputerChoice();
+        compareVsUser(par, computerChoice); //execute game logic
+        compChoice.innerHTML = computerChoice; //inform user of the computer choice
+        gameCountLabel.innerHTML = gameCount; //update game count label
+        userScoreLabel.innerHTML = userScore; //update user score label
+        computerScoreLabel.innerHTML = computerScore; //update computer score label
+    } else if (gameCount === 5 && userScore > computerScore) { //a message when the user wins
+        alert("You have won the game!")
+        location.reload();
+    } else if (gameCount === 5 && userScore < computerScore) { //a message when the user loses
+        alert("You have lost the game :(")
+        location.reload();
+    } else if (gameCount === 5 && userScore === computerScore) { //a message when it's a draw
+        alert("It's a draw!")
+        location.reload();
 
-    let userScore = 0;
-    let computerScore = 0;
-    let gameCount = 0;
-    let computerChoice = getComputerChoice();
-
-    function getComputerChoice() {
-        const availableChoices = ["Rock", "Paper", "Scissors"];
-        return availableChoices[Math.floor(Math.random() * 3)];
     }
-
-    function compareVsUser(playerSelection, computerSelection) {
-        if (playerSelection === "Rock" && computerSelection === "Rock") {
-            messageBanner.innerHTML = "It's a draw!";
-        } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-            messageBanner.innerHTML = "You Lose! Paper beats Rock";
-            computerScore++;
-        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-            messageBanner.innerHTML = "You Win! Rock beats Scissors";
-            userScore++;
-        } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-            messageBanner.innerHTML = "It's a draw!";
-        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-            messageBanner.innerHTML = "You Win! Paper beats Rock";
-            userScore++;
-        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-            messageBanner.innerHTML = "You Lose! Scissors beat Paper";
-            computerScore++;
-        } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-            messageBanner.innerHTML = "It's a draw!";
-        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-            messageBanner.innerHTML = "You Win! Scissors beat Paper";
-            userScore++;
-        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-            messageBanner.innerHTML = "You Lose! Rock beats Scissors";
-            computerScore++;
-        }
-    }
-
-    gameCount ++;
-    compareVsUser(par, computerChoice);
-    compChoice.innerHTML = computerChoice;
-    gameCountLabel.innerHTML = gameCount;
-    userScoreLabel.innerHTML = userScore;
-    computerScoreLabel.innerHTML = computerScore;
 
 }
